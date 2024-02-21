a0020c539103b7f77b3e22acaf9517d1f288b65c
hugomotadev <hugomotadev@gmail.com>
2024-02-21 10:02:02 -0300

    Update page file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main className="mt-[200px]">
-      <p className="font-bold text-center uppercase text-3xl bg-green-500 md:bg-red-500 lg:bg-green-500">Home</p>
+    <main className="">
+        <p className="font-bold text-center uppercase text-3xl bg-green-500 md:bg-red-500 lg:bg-green-500">
+          Home
+        </p>
     </main>
   );
 }
